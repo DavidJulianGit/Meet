fa97a644933be3dbf7f31580a2602f840caeeedb
DavidJulianGit <DavidJulianGit@users.noreply.github.com>
2024-04-04 13:31:08 +0200

    Ex. 4.4 - Unit tests for features 1-3.

---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const NumberOfEvents = () => {
+
+   const [numberOfEvents, setNumberOfEvents] = useState("32");
+
+   const handleInputChange = (event) => {
+      const value = event.target.value;
+      if (/^\d*$/.test(value)) {
+         setNumberOfEvents(value);
+      }
+   }
+
+   return (
+      <div id="numberOfEvents">
+         <input
+            type="text"
+            value={numberOfEvents}
+            onChange={handleInputChange}
+         />
+      </div>
+   );
+}
+
+export default NumberOfEvents;
